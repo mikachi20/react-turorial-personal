9cd2f5b83675299e97313064a35c14573796e272
mikachi20 <mKs1m400H@gmail.com>
2023-02-07 12:01:10 +0900

    カウンター作成

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import { List } from "./List";
+
 function App() {
-  return <div>ゼロから始めるReact入門</div>;
+  const [description, setDescription] = useState(0);
+  const changeDescription = () => {
+    setDescription(description + 1);
+  };
+  return (
+    <div>
+      {description}
+      <List title="取り扱い言語一覧" />
+      <button onClick={changeDescription}>ボタン</button>
+    </div>
+  );
 }
 
 export default App;
