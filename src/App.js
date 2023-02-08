3efa26abe883927215764c41a8f6ff0389ba84b4
mikachi20 <mKs1m400H@gmail.com>
2023-02-08 10:10:50 +0900

    class componentsの基礎

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { useState } from "react";
+import React from "react";
 import { List } from "./List";
 
-function App() {
-  const [description, setDescription] = useState(0);
-  const changeDescription = () => {
-    setDescription(description + 1);
-  };
-  return (
-    <div>
-      {description}
-      <List title="取り扱い言語一覧" />
-      <button onClick={changeDescription}>ボタン</button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: "クリック前の表示" };
+  }
+  changeDescription() {
+    this.setState({ description: "クリック後の表示です" });
+  }
+  render() {
+    const { description } = this.state;
+    return (
+      <div>
+        {description}
+        <List title="取り扱い言語一覧"></List>
+        <button onClick={() => this.changeDescription()}>ボタン</button>
+      </div>
+    );
+  }
 }
 
 export default App;
